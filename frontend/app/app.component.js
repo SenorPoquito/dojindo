fd9551dbfdf7e75cca04f0f74e333cfd4f9474ab
Yassir Al-Refaie <yassir_al-refaie@irep.co.jp>
2016-02-12 22:29:20 +0900

    AddedCollection Create Form

--- frontend/app/app.component.js
+++ frontend/app/app.component.js
@@ -1,5 +1,6 @@
-System.register(['angular2/core', './services/user.service', './components/user_list/user-list.component', './components/create-user/create-user.component', './components/collection_create_form/collection-create.component'], function(exports_1) {
+System.register(['angular2/core', './services/user.service', './components/user_list/user-list.component', './components/create-user/create-user.component', './components/collection_create_form/collection-create.component'], function(exports_1, context_1) {
     "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
